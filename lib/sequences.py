4f93274d57f8705a61b67676c4000e9a36b4a39d
John-thabuks <thabuksjohn@gmail.com>
2024-01-08 17:06:14 +0300

    Assignment completed

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,4 +1,21 @@
 #!/usr/bin/env python3
 
+# def print_fibonacci(length):
+#     sum;
+#     if len(length) > 2:
+#         sum = length[-1] + length[-2]
+
+#     return sum
+
 def print_fibonacci(length):
-    pass
+    if length <= 0:
+        print("Length should be a positive integer.")
+        return
+    
+    sequence = [0, 1]
+    
+    for _ in range(2, length):
+        number = sequence[-1] + sequence[-2]
+        sequence.append(number)
+    
+    print(sequence)
