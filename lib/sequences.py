9e822b1abe852db3c2654f65514fe8a88e7086b6
John-thabuks <thabuksjohn@gmail.com>
2024-02-21 14:30:02 +0300

    correct the error

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,21 +1,22 @@
 #!/usr/bin/env python3
 
-# def print_fibonacci(length):
-#     sum;
-#     if len(length) > 2:
-#         sum = length[-1] + length[-2]
-
-#     return sum
 
 def print_fibonacci(length):
-    if length <= 0:
-        print("Length should be a positive integer.")
-        return
-    
-    sequence = [0, 1]
-    
-    for _ in range(2, length):
-        number = sequence[-1] + sequence[-2]
-        sequence.append(number)
-    
-    print(sequence)
+
+    #initialise new list
+	sequence = []
+	
+    #cater for length between 0 and 1 only first.
+	if length > 0:
+		sequence.append(0)
+		
+    #cater for length equal to 2 going up second.
+	#firstly, if length = 2
+	if length >= 2:
+		sequence.append(1)
+		
+        #secondly, for length greater than 2
+		for i in range(2, length):
+			sequence.append(sequence[-1] + sequence[-2])
+
+	print(sequence)
